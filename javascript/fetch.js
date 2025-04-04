9042f1c6026cc6e30b1c8270a755ad1f90fc43c9
catsriel <danbro07@imkatta.no>
2025-04-04 13:35:41 +0200

    Finish basic logic
    All of the buttons are functional now :D

--- javascript/fetch.js
+++ javascript/fetch.js
@@ -1,7 +1,8 @@
 const oneJoke = document.getElementById("oneJoke");
 const inOrder = document.getElementById("inOrder");
 const allJokes = document.getElementById("twoJokes");
-const jokeText = document.getElementById("jokeText")
+const jokeText = document.getElementById("jokeText");
+const addText = document.getElementById("addText");
 
 let jsonFile = null; // JSON
 let length = 0; // Amount of entries within the JSON
@@ -12,7 +13,28 @@ function getJokes(){ // Get JSON with a Fetch Request
     .then((json) => { // use the json
         length = Object.keys(json).length; // Get the amount of keys in the json Object
         jsonFile = json; // Save the json to a Global variable 
+
+        createList()
     })
+}
+
+function createList(){
+    for(let i = 1; i < length; i++){ // Write the list of all the jokes
+        const newP = document.createElement("p");
+        newP.innerHTML = jsonFile[i]
+        console.log(":D")
+        addText.appendChild(newP)
+    }
+}
+
+function stateOfJokes(state){
+    if (state == true){ // Hide or show the jokeText div
+        jokeText.style.display = "inline"
+        addText.style.display = "none"
+    } else {
+        jokeText.style.display = "none"
+        addText.style.display = "inline"
+    }
 }
 
 function randomizeJoke(){
@@ -23,23 +45,31 @@ function randomizeJoke(){
 
 document.addEventListener("DOMContentLoaded", () => {
     getJokes();
+    addText.style.display = "none"
 })
 
 oneJoke.addEventListener("click", () => {
     jokeText.innerHTML = randomizeJoke()
+    stateOfJokes(true)
 })
 
 allJokes.addEventListener("click", () => {
-    
+    stateOfJokes(false)
+
+
 })
 
-let clicked = 1;
-inOrder.addEventListener("click", () => {
-    if(clicked == length){
+let clicked = 0;
+inOrder.addEventListener("click", () => { 
+    stateOfJokes(true)
+
+    if(clicked == length){ // Loop through the object
         clicked = 1
     } 
     else{ 
         clicked += 1;
     }
+
+    jokeText.innerHTML = jsonFile[clicked]
 
 })
